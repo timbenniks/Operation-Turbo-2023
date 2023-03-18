44a349de27b8826442fb603516a8aa09b758b1cd
Tim Benniks <tbenniks@gmail.com>
2023-03-19 00:00:29 +0100

    better article query

--- pages/api/articles.ts
+++ pages/api/articles.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { username, per_page } = req.query
-  const data = await fetch(`https://dev.to/api/articles?username=${username}&per_page=${per_page || 3}`)
+  const { username, per_page, collection_id } = req.query
+  const data = await fetch(`https://dev.to/api/articles?username=${username}&state=all${collection_id ? `&collection_id=${collection_id}` : ""}`)
 
   const result = await data.json()
 
@@ -12,9 +12,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       title: article.title,
       description: article.description,
       date: article.published_timestamp,
+      collection_id: article.collection_id,
       image: article.cover_image.split("w_1000/")[1]
     }
   })
 
-  res.status(200).json({ result: articles })
+  let finalResult = articles;
+
+  if (collection_id) {
+    finalResult = articles.filter(article => String(article.collection_id) === String(collection_id))
+  }
+
+  res.status(200).json({ result: finalResult.slice(0, per_page ? Number(per_page) : finalResult.length) })
 }

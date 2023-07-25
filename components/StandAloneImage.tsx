8dd5795f06c474625d02b538594e534f6d1c8587
Tim Benniks <tbenniks@gmail.com>
2023-07-25 11:01:43 +0200

    Added new prop to standalone image

--- components/StandAloneImage.tsx
+++ components/StandAloneImage.tsx
@@ -6,15 +6,36 @@ type StandAloneImageProps = ComponentProps<{
   image: Image[];
   title: string;
   spaceBelow: string;
+  clickForOriginal: boolean;
 }>;
 
 const StandAloneImage: React.FC<StandAloneImageProps> = ({
   image,
   title,
   spaceBelow,
+  clickForOriginal,
 }: StandAloneImageProps) => (
   <>
-    {image && image[0]?.publicId ? (
+    {clickForOriginal ? (
+      <a
+        href={image[0].url}
+        target="_blank"
+        className={`${spaceBelow ? "mb-8" : ""} block`}
+      >
+        <CldImage
+          width={image[0]?.width}
+          height={image[0]?.height}
+          src={image[0]?.publicId}
+          alt={image[0]?.alt || title}
+          rawTransformations={[image[0]?.transformation]}
+          loading="eager"
+          sizes="100vw"
+          className={`object-cover fancy-image`}
+        />
+
+        {title ? <p className="mt-2">{title}</p> : null}
+      </a>
+    ) : (
       <CldImage
         width={image[0]?.width}
         height={image[0]?.height}
@@ -25,7 +46,7 @@ const StandAloneImage: React.FC<StandAloneImageProps> = ({
         sizes="100vw"
         className={`object-cover fancy-image ${spaceBelow ? "mb-8" : ""}`}
       />
-    ) : null}
+    )}
   </>
 );
 
